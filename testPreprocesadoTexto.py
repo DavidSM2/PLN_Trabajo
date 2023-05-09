b0e9aa30aa871c07c4c39bc4dc596546da99a470
pifor <pifor@alumni.uv.es>
2023-05-09 14:08:05 +0200

    Algo es algo pero no va

--- testPreprocesadoTexto.py
+++ testPreprocesadoTexto.py
@@ -1,5 +1,9 @@
 from Model.PreprocesadoTexto import _normalizarTexto
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 texto = 'Esto es un tweet dë ejemplô@elvergalarga còn cárâr.#como dije.(te crees)'
 print('Sin normalizar ', texto)
 print('Normalizado ',_normalizarTexto(texto))
+
+bv = TfidfVectorizer(ngram_range=(1,2), min_df=2, norm=None)
+
